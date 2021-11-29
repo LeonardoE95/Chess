cb818cbb1dfc5742a9f02c83550f7138977614fa
LeonardoE95 <leonardotamiano95@gmail.com>
2021-11-29 08:56:05 +0100

    Started handling movement

--- src/main.c
+++ src/main.c
@@ -15,6 +15,7 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
@@ -88,6 +89,8 @@ typedef struct {
   Piece *board[BOARD_WIDTH][BOARD_HEIGHT];
   PieceType w_score[16];
   PieceType b_score[16];
+
+  Piece *selected_piece;
   int quit;
 } Game;
 
@@ -103,7 +106,7 @@ void destroy_piece(Piece *p);
 
 void render_game(SDL_Renderer *renderer, Game *game);
 void render_pieces(SDL_Renderer *renderer, Game *game);
-void render_piece(SDL_Renderer *renderer, Piece *p);
+void render_piece(SDL_Renderer *renderer, Piece *p, int selected);
 void render_board(SDL_Renderer *renderer);
 
 // ----------------------------------------
@@ -216,32 +219,132 @@ Piece *init_piece(PieceType t, Pos init_pos) {
   
   Piece *p = calloc(1, sizeof(Piece));
   p->pos = init_pos;
+  p->type = t;
   p->image = img_p(IMG_Load(TYPE2PNG[t]));
   
   return p;
-}
-
-void render_piece(SDL_Renderer *renderer, Piece *p) {
-  // was the piece already rendered?
-  if (!p->texture) {
-    p->texture = SDL_CreateTextureFromSurface(renderer, p->image);
-  }
-
-  
-  SDL_Rect chess_pos = {
-    (int) floorf(p->pos.x * CELL_WIDTH),
-    (int) floorf(p->pos.y * CELL_HEIGHT),
-    (int) floorf(CELL_WIDTH),
-    (int) floorf(CELL_HEIGHT),
-  };
-
-  SDL_RenderCopy(renderer, p->texture, NULL, &chess_pos);
 }
 
 void destroy_piece(Piece *p) {
   SDL_DestroyTexture(p->texture);
   SDL_FreeSurface(p->image);
   free(p);
+}
+
+void update_selected_piece(Game *game, Pos p) {
+  game->selected_piece = game->board[p.x][p.y];
+}
+
+int check_move(Game *game, Piece *p, Pos new_pos) {
+  // returns 1 if the piece p can move from its current position to
+  // new_pos, 0 otherwise.
+  Pos old_pos = p->pos;
+  int valid = 0;
+
+  int dx = new_pos.x - old_pos.x;
+  int dy = new_pos.y - old_pos.y;
+
+  switch(p->type) {
+  case B_PAWN:
+
+    // at the start the pawn can choose to move two squares below.
+    if (old_pos.y == 1 && dy == 2 && dx == 0) {
+      // TODO: check for collision/out of board positions
+      valid = 1;
+    }
+    
+    // in general however it can only move one square below.
+    else if (dy == 1 && dx == 0) {
+      // TODO: check for collision/out of board positions
+      valid = 1; 
+    }
+
+    // unless its trying to eat some piece using the diagonals.
+    else if (1) { 
+      // TODO: implement this case
+      // TODO: check for collision/out of board positions
+    }
+    
+    break;
+
+  case W_PAWN:
+
+    // at the start the pawn can choose to move two squares below.
+    if (old_pos.y == 6 && dy == -2 && dx == 0) {
+      // TODO: check for collision/out of board positions
+      valid = 1;
+    }
+    
+    // in general however it can only move one square below.
+    else if (dy == -1 && dx == 0) {
+      // TODO: check for collision/out of board positions
+      valid = 1;
+    }
+
+    // unless its trying to eat some piece using the diagonals.
+    else if (1) { 
+      // TODO: implement this case
+    }
+    
+    break;
+
+
+  case B_ROOK:
+  case W_ROOK:
+    if (dy == 0 || dx == 0) {
+      valid = 1;
+    }
+    
+    break;
+
+  case B_BISHOP:
+  case W_BISHOP:
+    if (abs(dx) == abs(dy)) {
+      valid = 1;
+    }
+    
+    break;
+
+  case B_KNIGHT:
+  case W_KNIGHT:
+    break;
+
+  case B_QUEEN:
+  case W_QUEEN:
+    break;
+
+  case B_KING:
+  case W_KING:
+    break;
+
+  case EMPTY:
+    break;
+
+  default:
+    fprintf(stderr, "[ERROR] - Default clause in check move!\n");
+    exit(1);
+    break;
+  }
+  
+  return valid;
+}
+
+void move_piece(Game *game, Piece *p, Pos new_pos) {
+  if(!check_move(&GAME, p, new_pos)) {
+    return;
+  }
+  
+  printf("valid move :D!\n");
+
+  // are we eating something?
+  if(game->board[new_pos.x][new_pos.y]) {
+    // TODO: handle this case
+  } else {
+    game->board[p->pos.x][p->pos.y] = NULL;
+    game->board[new_pos.x][new_pos.y] = p;
+    p->pos = new_pos;
+  }
+  
 }
 
 // ----------------------------------------
@@ -275,11 +378,35 @@ void render_board(SDL_Renderer *renderer) {
   }
 }
 
+void render_piece(SDL_Renderer *renderer, Piece *p, int selected) {
+  // was the piece already rendered?
+  if (!p->texture) {
+    p->texture = SDL_CreateTextureFromSurface(renderer, p->image);
+  }
+  
+  SDL_Rect chess_pos = {
+    (int) floorf(p->pos.x * CELL_WIDTH),
+    (int) floorf(p->pos.y * CELL_HEIGHT),
+    (int) floorf(CELL_WIDTH),
+    (int) floorf(CELL_HEIGHT),
+  };
+
+  SDL_RenderCopy(renderer, p->texture, NULL, &chess_pos);
+  
+  if (selected) {
+    // TODO: if the piece was selected we also have to indicate it
+    // somehow.
+  }
+  
+}
+
+
 void render_pieces(SDL_Renderer *renderer, Game *game) {
   for (int x = 0; x < BOARD_WIDTH; x++) {
     for (int y = 0; y < BOARD_HEIGHT; y++) {
       if (game->board[x][y]) {
-	render_piece(renderer, game->board[x][y]);
+	int selected = game->selected_piece == game->board[x][y];
+	render_piece(renderer, game->board[x][y], selected);
       }
     }
   }
@@ -287,7 +414,7 @@ void render_pieces(SDL_Renderer *renderer, Game *game) {
 
 // ----------------------------------------
 
-int main(void) {
+int main(void) {  
   // init classic SDL
   SDL_Init(SDL_INIT_VIDEO);
   SDL_Window *const window = sdl2_p(SDL_CreateWindow("Description", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE));  
@@ -305,6 +432,25 @@ int main(void) {
       if (event.type == SDL_QUIT) {
 	GAME.quit = 1;
       }
+
+      if (event.type == SDL_MOUSEBUTTONDOWN) {
+	Pos pos = (Pos){
+	  (int) floorf(event.button.x / CELL_WIDTH),
+	  floorf(event.button.y / CELL_HEIGHT)
+	};
+
+	// has the player started a movement?
+	if (!GAME.selected_piece) {
+	  update_selected_piece(&GAME, pos);
+	  printf("[INFO] - Player has picked piece at position: (%d, %d)\n", pos.x, pos.y);
+	} else {
+	  Piece *p = GAME.selected_piece;
+	  printf("[INFO] - Player wants to move piece %d from pos: (%d, %d) to pos (%d, %d)\n", p->type, p->pos.x, p->pos.y, pos.x, pos.y);
+	  move_piece(&GAME, p, pos);
+	  GAME.selected_piece = NULL;
+	}
+      }
+      
     }
 
     sdl2_c(SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255));
